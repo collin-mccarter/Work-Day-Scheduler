84e583fbdac6c1098481060bd0649b884efc5d76
collin-mccarter <collinmccarter12@gmail.com>
2023-03-11 16:19:59 -0600

    24 hr clock updates

--- Assets/script.js
+++ Assets/script.js
@@ -28,15 +28,16 @@ $(document).ready(function() {
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
-  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
-  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
-  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
-  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13")); // 1pm
+  $("#hour-14 .description").val(localStorage.getItem("hour-14")); // 2pm
+  $("#hour-15 .description").val(localStorage.getItem("hour-15")); // 3pm
+  $("#hour-16 .description").val(localStorage.getItem("hour-16")); // 4pm
+  $("#hour-17 .description").val(localStorage.getItem("hour-17")); // 5pm
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
 });
