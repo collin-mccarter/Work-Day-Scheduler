62ed1065b6e93a60086d3360443080e826deed8b
collin-mccarter <collinmccarter12@gmail.com>
2023-03-12 15:51:15 -0500

    finally working w/ comments

--- Assets/script.js
+++ Assets/script.js
@@ -6,31 +6,26 @@ $(document).ready(function() {
     // TODO: Add code to display the current date in the header of the page.
     var today = dayjs(); // reaches to dayJs to set today as a value
     $('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm A')); // changes & displays current day in visible text
-    var dateUpdated = function() {
-      document.getElementById('currentDay').innerHTML = dayjs().format('dddd, MMMM D, YYYY h:mm A');}
-      setInterval(dateUpdated, 1000);
+    var dateUpdated = function() { // function to update the page as time changes
+      document.getElementById('currentDay').innerHTML = dayjs().format('dddd, MMMM D, YYYY h:mm A'); // day js time format display in html ID
+    }
+    setInterval(dateUpdated, 1000); // update interval
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-  $(".saveBtn").on("click", function () { // pm click from save button class
-      console.log(this); // logs the data in console
-      var text = $(this).siblings(".description").val(); // gets input from description class and saves it as "value"
-      var time = $(this).parent().attr("id"); // gets the time and saves ID as a "key"
+    $(".saveBtn").on("click", function () { // pm click from save button class
+        console.log(this); // logs the data in console
+        var text = $(this).siblings(".description").val(); // gets input from description class and saves it as "value"
+        var time = $(this).parent().attr("id"); // gets the time and saves ID as a "key"
 
-      localStorage.setItem(time, text); // puts input from description and text into local storage and sorts it with key and value
-  })
+        localStorage.setItem(time, text); // puts input from description and text into local storage and sorts it with key and value
+    })
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));   // 9am
+  $("#hour-10 .description").val(localStorage.getItem("hour-10")); // 10am
+  $("#hour-11 .description").val(localStorage.getItem("hour-11")); // 11am
+  $("#hour-12 .description").val(localStorage.getItem("hour-12")); // 12pm
   $("#hour-13 .description").val(localStorage.getItem("hour-13")); // 1pm
   $("#hour-14 .description").val(localStorage.getItem("hour-14")); // 2pm
   $("#hour-15 .description").val(localStorage.getItem("hour-15")); // 3pm
@@ -43,26 +38,28 @@ $(document).ready(function() {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-   function timeChecker() {
-    var currentHour = dayjs().hour();
-    
-    $('.timeblock').each(function() {
-      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+   function timeChecker() { // function to compare time current time to time on table
+    var currentHour = dayjs().hour(); // getting the current hour and making it a value to compare against
+    // console.log(currentHour); 
+
+    $('.time-block').each(function() { // gets the class in timeblock and ->
+      var blockHour = parseInt($(this).attr('id').split('-')[1]); // sets the hour id and splits off the number, saves it as a variable value
+      // console.log(blockHour); 
 
       if (blockHour===currentHour) { // present
-      $(this).removeClass('past');
-      $(this).removeClass('future');
-      $(this).addClass('present');
+      $(this).removeClass('past'); // removes past css class if present
+      $(this).removeClass('future'); // removes future css class if present
+      $(this).addClass('present'); // adds present css class
       } else if (blockHour<currentHour) { // past
-      $(this).removeClass('future');
-      $(this).removeClass('present');
-      $(this).addClass('past');
+      $(this).removeClass('future'); // removes future css class if present
+      $(this).removeClass('present'); // removes present css class if present
+      $(this).addClass('past');  // adds past css class
       } else { // future
-      $(this).removeClass('present');
-      $(this).removeClass('past');
-      $(this).addClass('future');
+      $(this).removeClass('present'); // removes present css class if present
+      $(this).removeClass('past'); // removes past css class if present
+      $(this).addClass('future'); // adds future css class
       }
     })
   }
-  timeChecker();
+  timeChecker(); // executes function
 });
